62022861a0b2e50604ebd66217d8d5a101408e57
xieminhan <547100433@qq.com>
2020-07-20 00:11:47 +0800

    update01

--- src/main/java/tools/test/FirstDemo.java
+++ src/main/java/tools/test/FirstDemo.java
@@ -3,5 +3,7 @@ package tools.test;
 public class FirstDemo {
     public static void main(String[] args) {
         System.out.println("什么鬼");
+        System.out.println("什么鬼");
+        System.out.println("什么鬼");
     }
 }
